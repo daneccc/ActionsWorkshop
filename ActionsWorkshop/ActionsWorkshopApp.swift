bd9237c29c86eb2319c0a9eec6fcbc7dd3648d6f
Daniele Cavalcante <39858223+daneccc@users.noreply.github.com>
2023-08-09 09:09:04 -0300

    basic project

---
+++ ActionsWorkshop/ActionsWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionsWorkshopApp.swift
+//  ActionsWorkshop
+//
+//  Created by Daniele Cavalcante on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ActionsWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
